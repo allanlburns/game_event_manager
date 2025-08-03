b15cde50b03e8109b6402ea4b4bc9d09ebbe8150
Allan Burns <allanlburns.gmail.com>
2025-08-03 13:29:50 -0400

    First commit

---
+++ src/main/java/com/graycat/game_event_manager/GameEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.graycat.game_event_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameEventManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graycat/game_event_manager/GameEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graycat.game_event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
